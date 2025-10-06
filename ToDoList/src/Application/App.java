0e00d67e2964a2d534ab32ba1c6890ce1263037d
kaua <kauaxavier032@gmail.com>
2025-10-05 23:34:33 -0300

    feat: logic to select the type of task created

--- ToDoList/src/Application/App.java
+++ ToDoList/src/Application/App.java
@@ -6,8 +6,14 @@ package Application;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerDateModel;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 /**
  *
@@ -16,6 +22,10 @@ import java.util.List;
 public class App extends JFrame {
     private JTextField campoTitulo;
     private JTextField campoDescricao;
+    private JComboBox<String> selectTipo;
+    private JSpinner campoData;
+    String[] tipos = {"Tarefa Comum", "Tarefa Prazo", "Tarefa Rotina"};
+    private JTextField campoFrequencia;
     private JButton botaoAdicionar;
     private JTextArea listaTarefas;
     private ArrayList<TarefaModel.Tarefa> tarefas = new ArrayList<>();
@@ -23,25 +33,85 @@ public class App extends JFrame {
     public App(){
         campoTitulo = new JTextField(20);
         campoDescricao = new JTextField(20);
+        selectTipo = new JComboBox<>(tipos);
+
+        campoFrequencia = new JTextField(20);
+
+        campoData = new JSpinner(new SpinnerDateModel());
+        JSpinner.DateEditor editor = new JSpinner.DateEditor(campoData, "dd/MM/yyyy");
+        campoData.setEditor(editor);
+
         botaoAdicionar = new JButton("Adicionar");
+
         listaTarefas = new JTextArea(10, 30);
         listaTarefas.setEditable(false);
 
+        JPanel painelSuperior = new JPanel();
         JPanel painelEntrada = new JPanel();
+        JPanel painelEntrada2 = new JPanel();
         painelEntrada.add(new JLabel("Título:"));
         painelEntrada.add(campoTitulo);
         painelEntrada.add(new JLabel("Descrição:"));
         painelEntrada.add(campoDescricao);
-        painelEntrada.add(botaoAdicionar);
+
+        painelEntrada2.add(new JLabel("Tipo:"));
+        painelEntrada2.add(selectTipo);
+
+        JLabel freqLabel = new JLabel("Frequência:");
+        painelEntrada2.add(freqLabel);
+        painelEntrada2.add(campoFrequencia);
+
+        JLabel dataLabel = new JLabel("Data:");
+        painelEntrada2.add(dataLabel);
+        painelEntrada2.add(campoData);
+
+        painelEntrada2.add(botaoAdicionar);
+
+        campoData.setVisible(false);
+        dataLabel.setVisible(false);
+        campoFrequencia.setVisible(false);
+        freqLabel.setVisible(false);
+
+        selectTipo.addActionListener(e -> {
+            campoData.setVisible(false);
+            dataLabel.setVisible(false);
+            campoFrequencia.setVisible(false);
+            freqLabel.setVisible(false);
+            String Tipo = (String) selectTipo.getSelectedItem();
+            if(Tipo.equals("Tarefa Prazo")){
+                campoData.setVisible(true);
+                dataLabel.setVisible(true);
+            }else if(Tipo.equals("Tarefa Rotina")){
+                campoFrequencia.setVisible(true);
+                freqLabel.setVisible(true);
+            }
+        });
 
         botaoAdicionar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String titulo = campoTitulo.getText();
                 String descricao = campoDescricao.getText();
+                String Tipo = (String) selectTipo.getSelectedItem();
                 if(!titulo.isEmpty() && !descricao.isEmpty()) {
-                    TarefaModel.TarefaComum tarefa = new TarefaModel.TarefaComum(1, titulo, descricao);
-                    tarefas.add(tarefa);
+                    if(Tipo.equals("Tarefa Comum")){
+                        TarefaModel.TarefaComum tarefa = new TarefaModel.TarefaComum(1, titulo, descricao);
+                        tarefas.add(tarefa);
+                    }else if(Tipo.equals("Tarefa Rotina")){
+                        String frequencia = campoFrequencia.getText();
+                        TarefaModel.TarefaRotina tarefa = new TarefaModel.TarefaRotina(1, titulo, descricao, frequencia);
+                        tarefas.add(tarefa);
+                    }else if(Tipo.equals("Tarefa Prazo")){
+                        Date data = (Date) campoData.getValue();
+                        LocalDate dataTarefa = data.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                        TarefaModel.TarefaComPrazo tarefa = new TarefaModel.TarefaComPrazo(1, titulo, descricao, dataTarefa);
+                        tarefas.add(tarefa);
+                    }
+
+                    for(TarefaModel.Tarefa t: tarefas){
+                        t.exibirDetalhes();
+                    }
+
                     atualizarLista();
                     campoTitulo.setText("");
                     campoDescricao.setText("");
@@ -50,8 +120,11 @@ public class App extends JFrame {
         });
 
 
-        setLayout(new BorderLayout());
-        add(painelEntrada, BorderLayout.NORTH);
+        painelSuperior.setLayout(new BoxLayout(painelSuperior, BoxLayout.Y_AXIS));
+        painelSuperior.add(painelEntrada);
+        painelSuperior.add(painelEntrada2);
+
+        add(painelSuperior, BorderLayout.NORTH);
         add(new JScrollPane(listaTarefas), BorderLayout.CENTER);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
